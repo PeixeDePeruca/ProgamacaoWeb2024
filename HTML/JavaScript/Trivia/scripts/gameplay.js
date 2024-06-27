b5cb41ccfc58a3fd9ee15f5b9a3edeee3be1f8f3
PeixeDePeruca <twitterluizin@gmail.com>
2024-06-27 17:32:52 -0300

    commit 27/06/2024 terminar

--- HTML/JavaScript/Trivia/scripts/gameplay.js
+++ HTML/JavaScript/Trivia/scripts/gameplay.js
@@ -1,106 +1,94 @@
-
-
-// Alternativas de temas:
-// Programas de TV / Novelas / Filmes / etc...
-// Séries de sites pagos com streaming de video
-// Músicas / Estilos musicais
-// Esportes
-// Jogos / Animes
-
-questions = [
+const questions = [
     {
-        // MJ
-        question : 'Qual é o estilo musical ?',
-        a : 'Jazz',
-        b : 'Pop',
-        c : 'MPB',
-        d : 'Samba',
-        answer : 'b'
+        question: 'Qual é o estilo musical?',
+        a: 'Jazz',
+        b: 'Pop',
+        c: 'MPB',
+        d: 'Samba',
+        answer: 'b'
     },
     {
-        // K F P
-        question : 'Quem é o cantor?',
-        a : 'Stevie Wonder',
-        b : 'James Hetfield',
-        c : 'Eddie Vedder',
-        d : "Jack Black",
-        answer : 'd'
-    },
-    {   
-        // Ka
-        question : 'Qual é o estilo musical ?',
-        a : 'Jazz',
-        b : 'Pop',
-        c : 'MPB',
-        d : 'Samba',
-        answer : 'c'
+        question: 'Quem é o cantor?',
+        a: 'Stevie Wonder',
+        b: 'James Hetfield',
+        c: 'Eddie Vedder',
+        d: "Jack Black",
+        answer: 'd'
     },
     {
-        // I U 2
-        question : 'Essa música vem de qual filme?',
-        a : 'Velozes e Furiosos 3 Desafio em Tóquio',
-        b : 'Irmão Urso 2',
-        c : 'A era do Gelo 3',
-        d : 'Rio 2',
-        answer : 'b'
+        question: 'Qual é o estilo musical?',
+        a: 'Jazz',
+        b: 'Pop',
+        c: 'MPB',
+        d: 'Samba',
+        answer: 'c'
     },
     {
-        // M C Ma
-        question : 'Quem é o cantor?',
-        a : 'Tim Maia',
-        b : 'MC Marcinho',
-        c : 'Padre Fabio de Melo',
-        d : 'Gal Costa',
-        answer : 'a'
+        question: 'Essa música vem de qual filme?',
+        a: 'Velozes e Furiosos 3 Desafio em Tóquio',
+        b: 'Irmão Urso 2',
+        c: 'A era do Gelo 3',
+        d: 'Rio 2',
+        answer: 'b'
     },
     {
-        //
-        question : 'Essa fala é o bordão de qual personagem?',
-        a : 'Jack Sparrow',
-        b : 'Jake o Cão',
-        c : 'Peter Griffin',
-        d : 'Homer Simpson',
-        answer : 'd'
+        question: 'Quem é o cantor?',
+        a: 'Tim Maia',
+        b: 'MC Marcinho',
+        c: 'Padre Fabio de Melo',
+        d: 'Gal Costa',
+        answer: 'a'
+    },
+    {
+        question: 'Essa fala é o bordão de qual personagem?',
+        a: 'Jack Sparrow',
+        b: 'Jake o Cão',
+        c: 'Peter Griffin',
+        d: 'Homer Simpson',
+        answer: 'd'
     }
 ];
 
-
-
-var round = 1;
+let round = 1;
 const roundHTML = document.getElementById('round');
-const question = document.getElementById('question'); 
+const question = document.getElementById('question');
 const altA = document.getElementById('altA');
 const altB = document.getElementById('altB');
 const altC = document.getElementById('altC');
 const altD = document.getElementById('altD');
-
+let timerID = undefined;
 
 function updateInfos() {
-    roundHTML.textContent = round;
-    question.textContent = questions[round-1].question;
-    altA.textContent = questions[round-1].a;
-    altB.textContent = questions[round-1].b;
-    altC.textContent = questions[round-1].c;
-    altD.textContent = questions[round-1].d;
+    question.textContent = questions[round - 1].question;
+    altA.textContent = questions[round - 1].a;
+    altB.textContent = questions[round - 1].b;
+    altC.textContent = questions[round - 1].c;
+    altD.textContent = questions[round - 1].d;
+
+    timerID = setTimeout(timeOut, 5000);
+}
+
+function answer(alt) {
+    clearTimeout(timerID);
+
+    if (alt === questions[round - 1].answer) {
+        console.log('Acertou');
+
+        if (round < questions.length) {
+            round++;
+            updateInfos();
+        } else {
+            alert('Acabaram as questões');
+            window.location.replace('../html/index.html');
+        }
+    } else {
+        console.log('Errou');
+    }
+}
+
+function timeOut() {
+    alert('O tempo acabou.');
+    window.location.replace('../html/index.html');
 }
 
 updateInfos();
-
-// A variável "alt" pode ter os valores : 'a' , 'b' , 'c' ou 'd'
-function answer(alt) {
-    // Verificar se "alt" é igual à resposta da questão.
-    // Se a resposta for SIM, então podemos incrementar a variável "rounds" e atualizar a interface novamente.
-    // Se a resposta for NÃO, podemos encerrar o jogo ou decrementar o número de tentativas. (Tentativas são opcionais)
-
-    if ( alt == questions[round-1].answer ) {
-        console.log('Acertou');
-        round = round + 1;
-        updateInfos();
-
-    }
-    else {
-        console.log('Errou');
-    }
-
-
-}
