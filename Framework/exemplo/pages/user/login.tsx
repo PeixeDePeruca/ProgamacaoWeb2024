a21a9cc23b7de0090688af0ec3dd744c1da9e911
PeixeDePeruca <twitterluizin@gmail.com>
2024-07-26 17:50:31 -0300

    commit site 26/07/24
    isso aí

---
+++ Framework/exemplo/pages/user/login.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import styles from "@/styles/login.module.css";
+
+export default function Login() {
+
+    return (
+        <main>
+            <head>
+                <title>Login</title>
+            </head>
+
+            <form>
+                <input type="text" placeholder="Login" />
+                <input type="text" placeholder="Password" />
+
+                <input type="submit" value="Enter" />
+            </form>
+        name 
+        username
+        password
+        ConfirmPassword
+
+
+
+
+        </main>
+    );
+}
--- Framework/exemplo/pages/user/register.tsx
+++ Framework/exemplo/pages/user/register.tsx
@@ -1,24 +1,78 @@
 // pages/register.tsx
 import Head from "next/head";
 import styles from '@/styles/register.module.css';
+import { useState } from "react";
+import Link from "next/link";
+
 
 export default function Register() {
+
+    const [ formData , setFormData ] = useState({
+        
+            name: "",
+            email: "",
+            username: "",
+            password: "",
+            ConfirmPassword: "" 
+    }
+
+    );
+
+    function handleFormEdit(event:any , field:string) {
+        setFormData({
+            ...formData,
+            [field]: event.target.value
+        });
+
+        console.log(formData);
+
+    }
+
+    async function formSubmit(event:any) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`/api/action/user/create` , {
+                method: 'POST',
+                headers: {'Content-type':'application/json'},
+                body: JSON.stringify(formData)
+            });
+
+            const respondeJson = await response.json();
+
+            alert(`${response.status} \n ${respondeJson}`);
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+
+
     return (
-        <main className={styles.background}>
+        <main id={styles.background} className="flex min-h-screen flex-col" >
             <Head>
                 <title>Cadastro</title>
             </Head>
+
             <h1 className={styles.text}>PÁGINA  DE  CADASTRO</h1>
-            <form className={styles.container}>
+
+            <form className={styles.container} onSubmit={formSubmit}>
  
-                <input className={styles.input} type="text" placeholder="Nome" /><br />
-                <input className={styles.input} type="email" placeholder="Email" /><br />
-                <input className={styles.input} type="text" placeholder="Nome de Usuário" /><br />
-                <input className={styles.input} type="password" placeholder="Senha" /><br />
-                <input className={styles.input} type="password" placeholder="Confirmação da senha" /><br />
+            
+                <input className={styles.input} type="text" placeholder="Nome (Opcional)" onChange={(event) => {handleFormEdit(event , "name")}}  /><br />
+                <input className={styles.input} type="email" placeholder="Email" onChange={(event) => {handleFormEdit(event , "email")}}  /><br />
+                <input className={styles.input} type="text" placeholder="Nome de Usuário" onChange={(event) => {handleFormEdit(event , "username")}}  /><br />
+                <input className={styles.input} type="password" placeholder="Senha" onChange={(event) => {handleFormEdit(event , "password")}}  /><br />
+                <input className={styles.input} type="password" placeholder="Confirmação da senha" onChange={(event) => {handleFormEdit(event , "ConfirmPassword")}}  /><br />
  
+                <input className={styles.CreateBtn} type="submit" value="Criar Conta" />
+                <br />
+                <Link className={styles.LoginBtn}   href={`/user/login`}>Já tenho uma conta</Link>
             </form>
- 
+
+            
 
         </main>
     );
